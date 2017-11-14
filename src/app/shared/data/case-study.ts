b95a156eabe58dc78e73f14c526d41c9af6909b7
Pankaj Rawat <pankajrawat19sept@gmail.com>
2017-11-14 17:49:13 +0530

    updated case study link

--- src/app/shared/data/case-study.ts
+++ src/app/shared/data/case-study.ts
@@ -4,13 +4,13 @@ export const CASE_STUDY_DATA = [
     heading: 'The best warehouse management system',
     img: 'groovepacker-landing.jpg',
     color: {bg: 'linear-gradient(to bottom right, #ffc09b, #ed3722)', text: '#AA2525'},
-    link: '/work/groovepacker'
+    link: '/assets/case-study/groovepacker/pdf-study.pdf'
   },
   {
     name: 'My Veeta',
     heading: `Find your dream Job`,
     img: 'my-veeta-landing.jpg',
     color: {bg: 'linear-gradient(to right bottom, rgb(171, 243, 240), rgb(4, 113, 113))', text: '#167884'},
-    link: '/work/my-veeta'
+    link: '/assets/case-study/my-veeta/pdf-study.pdf'
   }
 ];
